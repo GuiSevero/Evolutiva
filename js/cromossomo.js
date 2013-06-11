0efb38e6cb8dac992885400f6e6ae631d05070f8
Guilherme Severo <grsevero@gmail.com>
2013-06-11 12:53:52 -0300

    mutate
    - FIX - Adição de Genes no cromossomo
    - FIX - log de atividades
    - ADD - Função de teste de cromossomo
    - ADD - Função de mutação do cromossomo
    - ADD - Função de geração da população inicial

--- js/cromossomo.js
+++ js/cromossomo.js
@@ -1,11 +1,63 @@
 function Cromossomo(num_horarios){
 
-	this._horarios = array(num_horarios); //Array com arrays de genes
+	this._horarios = Array(num_horarios); //Array com arrays de genes
 	this.val = -1;
 	this.isValid = false;
+	this.gens = Array();	
+
+	this.generate = function(gens){
+
+			 this.init(this._horarios.length);
+
+			this.gens = jQuery.extend({}, gens);
+
+			for(var g in this.gens){
+				//Gera um numero aleatório entre 0 e num_horario
+				var t = Math.floor((Math.random()*this._horarios.length));				
+
+				this._horarios[t].push(this.gens[g]); //clona o objeto para o horario
+			}
+
+			this.eval();
+
+	};
+
+	this.init = function(num){
+		for(i=0; i < num; i++ ){
+			this._horarios[i] = Array();
+		}
+	};
+
+
+	this.test = function(){
+		for(i=0; i<this._horarios.length; i++){
+			console.log(this._horarios[i]);
+		}
+
+	}
 
 	//Muta o cromossomo
-	this.mutate = function(){};
+	//Altera uma turma de horário
+	this.mutate = function(){
+
+		for(var i in this._horarios){
+
+			//Verifica o primeiro horário que tiver uma turma
+			if(this._horarios.length > 0){
+
+				var t = Math.floor((Math.random()*this._horarios.length));
+				//remove a primeira turma e coloca em um lugar randômico
+				this._horarios[t].push(this._horarios[i].pop());
+
+				//Reavalia o cromossomo
+				this.eval();
+
+				break;
+			}
+		}
+
+		return this;
+	};
 
 	//Avalia o cromossomo
 	this.eval = function(){};
@@ -13,11 +65,8 @@ function Cromossomo(num_horarios){
 	//Troca gens de lugar
 	this.swap = function(){};
 
+
 }
-
-
-
-
 
 /*
 *
--- js/ga.js
+++ js/ga.js
@@ -1,24 +1,36 @@
 function GA(){
 
+	//Gens Iniciais
+	this.gens = Array();
+
 	this.timeElapsed = 0;
 	this.timeOut = 0;
-	this._population = array();
+	this._population = Array();
 
-	this._populationSize = 100;
+	this._populationSize = 1;
 	this._eliteSize = 25;
 
 	this.mutationRate = 0.5;
 	this.steps = 0;
-
-	this.disciplinas = array();
-	this.professores = array();
 
 	this.num_horarios = 4 * 7; 
 
 	this.canStop = false;
 
 
-	this.generatePop = function(){};
+	this.generatePop = function(){
+
+		for(i=0; i < this._populationSize; i++){
+			cromossomo = new Cromossomo(this.num_horarios);
+			cromossomo.generate(this.gens);
+			this._population.push(cromossomo);	
+		}
+			
+		console.log("_population");
+		console.log(this._population);
+		
+
+	};
 
 	this.crossOver = function(){};
 
